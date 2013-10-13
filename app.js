d3e9044dcb57efdf906e9b28d27044438ba71669
fauzi <jipaw@Mylinux.(none)>
2013-10-13 23:24:47 +0700

    fixed layout folder structure

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 /**
  * Module dependencies.
  */
-//use strict
+'use strict';
 
 var express = require('express')
   , http = require('http')
@@ -12,7 +12,7 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.locals.pretty = true;
 app.use(express.favicon());
@@ -22,7 +22,7 @@ app.use(express.cookieParser());
 app.use(express.session({ secret: 'super-duper-secret-secret' }));
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './server/public')));
 
 
 // development only
